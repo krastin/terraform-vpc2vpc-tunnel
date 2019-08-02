5b786279f2c900a3681e15d526301f159fd39bf2
Krastin Krastev <krastin@hashicorp.com>
2019-08-02 15:28:46 +0200

    added vpn connection

--- us-east-1/vpn.tf
+++ us-east-1/vpn.tf
@@ -24,6 +24,17 @@ resource "aws_customer_gateway" "krastin-vpc1-cgw1" {
   }
 }
 
+resource "aws_vpn_connection" "krastin-vpc1-vpnconn1" {
+  vpn_gateway_id      = "${aws_vpn_gateway.krastin-vpc1-vpngw1.id}"
+  customer_gateway_id = "${aws_customer_gateway.krastin-vpc1-cgw1.id}"
+  type                = "ipsec.1"
+  static_routes_only  = true
+
+  tags = {
+      Name = "krastin-vpc1-vpnconn1"
+  }
+}
+
 /*
 resource "aws_vpc_peering_connection" "peering_vpc1_vpc2" {
   #peer_owner_id = "${var.peer_owner_id}"
